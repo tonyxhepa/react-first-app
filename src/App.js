9f9c0d4137c273e1e810204d2b695e3da7b233f4
laraveller <laraveller2021@gmail.com>
2022-06-29 16:39:36 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -35,17 +35,56 @@ const App = () => {
 
   return (
     <div className="max-w-6xl mx-auto bg-slate-200">
-      <h1 className="flex justify-center text-2xl font-semibold">
-        Welcome to laraveller.
-      </h1>
-      <div className="flex justify-center">
-        <input
-          className="m-2 p-2 text-gray-400 rounded-md"
-          type="search"
-          placeholder="search movies"
-          onChange={searchMoviesHandler}
-        />
+      <div class="relative flex items-center justify-between mb-8 h-16 bg-yellow-500">
+        <div class="flex items-center px-2 lg:px-0">
+          <Link to={"/"} className="flex ml-2 text-2xl font-semibold">
+            React Movie
+          </Link>
+          <div class="hidden lg:block lg:ml-2">
+            <div class="flex">
+              <Link
+                to={`/about`}
+                className="ml-4 px-3 py-2 rounded-md text-sm leading-5 text-gray-800 font-semibold hover:bg-yellow-500 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700"
+              >
+                About
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="flex-1 flex justify-center px-2 lg:ml-6 lg:justify-end">
+          <div class="max-w-lg w-full lg:max-w-xs">
+            <label for="search" class="sr-only">
+              Search{" "}
+            </label>
+            <form methode="get" action="#" class="relative z-50">
+              <button
+                type="submit"
+                id="searchsubmit"
+                class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+              >
+                <svg
+                  class="h-5 w-5 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <input
+                type="search"
+                class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-yellow-200 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
+                placeholder="Search"
+                onChange={searchMoviesHandler}
+              />
+            </form>
+          </div>
+        </div>
       </div>
+
       <Routes>
         <Route path="/" element={renderMovies} />
         <Route path="about" element={<About />} />
--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -16,19 +16,55 @@ const MovieDetails = () => {
   }, []);
   const { id, Poster, Title, Year, Runtime } = movie;
   return (
-    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex justify-center pt-3">
-        <img className="rounded-t-lg" src={Poster} alt={Title} />
-      </div>
-      <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {Title}
-        </h5>
+    <section className="bg-white dark:bg-gray-900 m-6 p-4">
+      <div className="container flex flex-col items-center px-4 py-12 mx-auto xl:flex-row">
+        <div className="flex justify-center xl:w-1/2">
+          <img
+            className="h-80 w-80 sm:w-[28rem] sm:h-[28rem] flex-shrink-0 object-cover rounded-md"
+            src={Poster}
+            alt={Title}
+          />
+        </div>
 
-        <span className="mr-4">{Year}</span>
-        <span>{Runtime}</span>
+        <div className="flex flex-col items-center mt-6 xl:items-start xl:w-1/2 xl:mt-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-800 xl:text-4xl dark:text-white">
+            {Title}
+          </h2>
+
+          <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
+            recusandae tenetur iste quaerat voluptatibus quibusdam nam
+            repudiandae{" "}
+          </p>
+          <span className="m-2 p-2 bg-slate-300 text-slate-800 rounded-md">
+            {Year}
+          </span>
+          <span className="m-2 p-2 bg-slate-300 text-slate-800 rounded-md">
+            {Runtime}
+          </span>
+
+          <div className="mt-6 sm:-mx-2">
+            <div className="inline-flex w-full overflow-hidden rounded-lg shadow sm:w-auto sm:mx-2">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-600 hover:to-gray-600 sm:w-auto"
+              >
+                <span className="mx-2">watch online</span>
+              </a>
+            </div>
+
+            <div className="inline-flex w-full mt-4 overflow-hidden rounded-lg shadow sm:w-auto sm:mx-2 sm:mt-0">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center w-full px-5 py-3 text-base font-medium text-white transition-colors duration-150 transform sm:w-auto bg-gradient-to-r from-blue-700 to-blue-900 hover:from-blue-600 hover:to-blue-600"
+              >
+                <span className="mx-2">download</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
